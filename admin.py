f7802bdf7c05c3209ccd84a00a2f79d91b6d79c7
mejomba <moojaminzadeh@gmail.com>
2023-01-31 21:58:59 +0330

    feat: add create superuser

--- admin.py
+++ admin.py
@@ -1,5 +1,18 @@
 from metro import User
+from sqlite3_contextmanager import CreateSuperUserContextManager as CreateSuperUser
 
 
-class Admin(User):
-    pass
+def create_super_user():
+    first_name = input("first name: ")
+    last_name = input("last name: ")
+    password = input("password: ")
+    phone = input("phone: ")
+    email = input("email: ")
+    with CreateSuperUser() as cu:
+        cu.create_superuser(first_name, last_name, password, phone, email, role=2)
+        cu.insert_to_database()
+
+    if cu.err:
+        print(cu.err)
+    elif cu.result:
+        print(cu.result)
